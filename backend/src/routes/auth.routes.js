72fde3baa600171b7e2b088f5b2edf9467fc5d53
Sachin Sen <sachinsen1920@gmail.com>
2025-08-14 11:05:15 +0530

    update routes

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,11 +1,14 @@
 import { Router } from 'express';
-import { registerUser } from '../controllers/auth.controllers.js';
+import { registerUser, loginUser } from '../controllers/auth.controllers.js';
 import { validate } from '../middlewares/validator.middleware.js';
-import { userRegisterValidator } from '../validators/index.js';
+import {
+    userRegisterValidator,
+    userLoginValidator,
+} from '../validators/index.js';
 
 const router = Router();
 
 router.post('/register', userRegisterValidator(), validate, registerUser);
-router.post('/login', registerUser);
+router.post('/login', userLoginValidator(), validate, loginUser);
 
 export default router;
