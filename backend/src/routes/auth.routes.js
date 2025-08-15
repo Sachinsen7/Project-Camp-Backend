503739320c924d0991c675e58481bc6502f00779
Sachin Sen <sachinsen1920@gmail.com>
2025-08-15 14:26:06 +0530

    add all auth routes

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -3,19 +3,54 @@ import {
     registerUser,
     loginUser,
     logoutUser,
+    verifyEmail,
+    refreshAccessToken,
+    forgotPasswordRequest,
+    resetForgotPassword,
+    getCurrentUser,
+    changeCurrentPassword,
+    resendEmailVerification,
 } from '../controllers/auth.controllers.js';
 import { validate } from '../middlewares/validator.middleware.js';
 import {
     userRegisterValidator,
     userLoginValidator,
+    userForgotPasswordValidator,
+    userResetForgotPasswordValidator,
+    userChangeCurrentPasswordValidator,
 } from '../validators/index.js';
 import verifyJWT from '../middlewares/auth.middleware.js';
 
 const router = Router();
 
+// unsecured routes
 router.post('/register', userRegisterValidator(), validate, registerUser);
 router.post('/login', userLoginValidator(), validate, loginUser);
+router.get('/verify-email/:verificationToken', verifyEmail);
+router.post('/refresh-token', refreshAccessToken);
+router.post(
+    '/forgot-password',
+    userForgotPasswordValidator(),
+    validate,
+    forgotPasswordRequest,
+);
+router.post(
+    '/reset-password/:resetToken',
+    userResetForgotPasswordValidator(),
+    validate,
+    resetForgotPassword,
+);
+
 //secure
 router.post('/logout', verifyJWT, logoutUser);
+router.post('/current-user', verifyJWT, getCurrentUser);
+router.post(
+    '/change-password',
+    verifyJWT,
+    userChangeCurrentPasswordValidator(),
+    validate,
+    changeCurrentPassword,
+);
+router.post('/resend-email-verification', verifyJWT, resendEmailVerification);
 
 export default router;
